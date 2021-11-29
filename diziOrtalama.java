362c0e7dca7b67007b6b5299deba043846c55d78
batuhanbelli <36133928+batuhanbelli@users.noreply.github.com>
2021-11-29 19:24:07 +0300

    Add files via upload

---
+++ diziOrtalama.java
@@ -0,0 +1,13 @@
+package Diziler;
+
+public class diziOrtalama {
+    public static void main(String[] args) {
+        int[] list = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        for (int i = 0; i < list.length; i++) {
+            sum += list[i];
+        }
+        System.out.println(sum / list.length);
+    }
+
+}
